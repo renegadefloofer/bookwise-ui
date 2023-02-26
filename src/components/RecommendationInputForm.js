13cec8936082c47de1aab2c3801eb4295c6bdc5a
vivek <566976656b@gmail.com>
2023-02-26 10:43:27 -0500

    wip

--- src/components/RecommendationInputForm.js
+++ src/components/RecommendationInputForm.js
@@ -1,14 +1,82 @@
+import { useState } from 'react';
 import styles from './RecommendationInputForm.module.css';
 
+//Google Books API
+const API_KEY = 'AIzaSyBVbgl1OrSUZy8SXDUKAtwYZDmFASE7axM';
+
 const RecommendationInputForm = ({ promptID }) => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  //const [searchParams, setSearchParams] = useState([]);
+
+  const titleChangeHandler = (event) => {
+    console.log(event.target.value);
+    //search for books here
+    setTitle(event.target.value);
+  };
+
+  const authorChangeHandler = (event) => {
+    console.log(event.target.value);
+    //search for books here
+    setAuthor(event.target.value);
+  };
+
+  // const bookSearchHandler = () => {
+  //   console.log('sbumit');
+  // };
+
+  const addBookHandler = () => {
+    console.log('add book');
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    const raw = {
+      prompt_id: promptID,
+      title: title,
+      author: author,
+    };
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    };
+
+    fetch(`https://app.arsolutions.it/api/v1/book`, requestOptions).then(
+      (res) => {
+        console.log(res);
+        res.json().then((data) => {
+          if (data['success']) {
+            setTitle('');
+            setAuthor('');
+          } else {
+            console.log('Error');
+          }
+        });
+      }
+    );
+  };
+
   return (
     <div className={styles['form-container']}>
       <h3 className={styles['heading']}>Recommend a Book</h3>
       <form>
-        <label htmlFor="title">Title</label>
-        <input type="text"></input>
-        <label htmlFor="author">Author</label>
-        <input type="text"></input>
+        <div className={styles['form-control']}>
+          <label htmlFor="title">Title</label>
+          <input type="text" onChange={titleChangeHandler}></input>
+        </div>
+        <div className={styles['form-control']}>
+          <label htmlFor="author">Author</label>
+          <input type="text" onChange={authorChangeHandler}></input>
+        </div>
+        <div className={styles['form-action']}>
+          <button
+            className={styles['btn']}
+            type="button"
+            onClick={addBookHandler}
+          >
+            Add
+          </button>
+        </div>
       </form>
     </div>
   );
